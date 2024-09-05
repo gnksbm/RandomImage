cb5f866a3ac0555890c54abcdc6a1fb97c4d0d96
Geonseob Kim <gnksbm@gmail.com>
2024-09-05 18:40:44 +0900

    Initial Commit

---
+++ RandomImage/RandomImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomImageApp.swift
+//  RandomImage
+//
+//  Created by gnksbm on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
